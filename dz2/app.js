44bab2b75ee19165d800b8293d54db70873c83a1
matvey <matveyproyt12272008@gmail.com>
2023-02-17 22:11:14 +0600

    dz3

--- dz2/app.js
+++ dz2/app.js
@@ -6,11 +6,11 @@ let positionX = 0
 let positionY = 0
 
 const move = () => {
-    if (positionX <= 449 && positionY === 0) {
+    if (positionX <= 2099 && positionY === 0) {
         positionX++
         box.style.left = `${positionX}px`
         setTimeout(move, 1)
-    } else if (positionX >= 449 && positionY <= 449) {
+    } else if (positionX >= 2099 && positionY <= 699) {
         positionY++
         box.style.top = `${positionY}px`
         setTimeout(move, 1)
@@ -27,24 +27,57 @@ const move = () => {
 
 move()
 
-// 2-ое задание
 
-let num = 0
 
-const count = () => {
-    let span = document.querySelector('.num')
-    num++
-    span.innerHTML = `${num}`
+const deadline = '2023-06-01'
+
+
+function getTimeRemaining(deadline) {
+    const time = new Date(deadline) - new Date(),
+        days = Math.floor((time / (1000 * 60 * 60 * 24))),
+        hours = Math.floor((time / (1000 * 60 * 60) % 24)),
+        minutes = Math.floor((time / 1000 / 60) % 60),
+        seconds = Math.floor((time / 1000) % 60)
+
+    return {
+        'total': time,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    }
 }
 
-document.querySelector('.start').onclick = () => {
-    let _1 = setInterval(count, 150);
-    let stopped = function () {
-        document.querySelector('.stop').onclick = () => {
-            clearInterval(_1)
+function setClock(element, deadline) {
+    const elem = document.querySelector(element),
+        days = elem.querySelector('#days'),
+        hours = elem.querySelector('#hours'),
+        minutes = elem.querySelector('#minutes'),
+        seconds = elem.querySelector('#seconds')
+
+    setInterval(updateClock, 1000)
+
+    updateClock()
+
+    function updateClock() {
+        const time = getTimeRemaining(deadline)
+        days.innerHTML = makeZero(time.days)
+        hours.innerHTML = makeZero(time.hours)
+        minutes.innerHTML = makeZero(time.minutes)
+        seconds.innerHTML = makeZero(time.seconds)
+    }
+
+    function makeZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`
+        } else {
+            return num
         }
     }
-    stopped()
+
 }
+
+setClock('.timer', deadline)
+
 
 
